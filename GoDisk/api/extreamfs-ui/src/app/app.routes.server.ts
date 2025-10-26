99083fb1aaa21af41b3036372bf2b798834d7f2c
Ángel Andrés Godoy Valdéz <110713221+AGODOYV37@users.noreply.github.com>
2025-10-26 14:38:05 -0600

    docker

--- GoDisk/api/extreamfs-ui/src/app/app.routes.server.ts
+++ GoDisk/api/extreamfs-ui/src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  // ✅ SOLO estas rutas estáticas se prerenderán:
+  { path: '',              renderMode: RenderMode.Prerender },
+  { path: 'login',         renderMode: RenderMode.Prerender },
+
+  // 🚫 NO prerender para páginas que hacen llamadas a /api:
+  { path: 'visualizador',        renderMode: RenderMode.Server },
+  { path: 'visualizador/:id',    renderMode: RenderMode.Server },
+  { path: 'visualizador/:id/fs', renderMode: RenderMode.Server },
+
+  // Wildcard sin prerender
+  { path: '**',            renderMode: RenderMode.Server },
 ];
+    
--- GoDisk/api/extreamfs-ui/src/app/app.routes.ts
+++ GoDisk/api/extreamfs-ui/src/app/app.routes.ts
@@ -1,23 +1,28 @@
 import { Routes } from '@angular/router';
 import { Consola } from './features/consola/consola';
 import { LoginComponent } from './features/login/login';
-import { VisualizadorComponent } from './features/visualizador/discos';
-
 
 export const routes: Routes = [
-  { path: '', component: Consola},
+  { path: '', component: Consola },
   { path: 'login', component: LoginComponent },
+
   {
     path: 'visualizador',
     loadComponent: () =>
       import('./features/visualizador/discos').then(m => m.VisualizadorComponent)
   },
+
   {
     path: 'visualizador/:id',
     loadComponent: () =>
       import('./features/visualizador/part').then(m => m.VisualizadorPartComponent)
   },
-  { path: 'visualizador/:id/fs', loadComponent: () => import('./features/visualizador/fs').then(m => m.FsExplorerComponent) },
 
-    { path: '**', redirectTo: '' }
+  {
+    path: 'visualizador/:id/fs',
+    loadComponent: () =>
+      import('./features/visualizador/fs').then(m => m.FsExplorerComponent)
+  },
+
+  { path: '**', redirectTo: '' }
 ];
